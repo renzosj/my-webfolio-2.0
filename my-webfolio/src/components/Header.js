e36c654cafa33ec2675dbd93362bc3e7e306002a
Renzo San Juan <rcsanjuan96@gmail.com>
2023-12-19 11:01:18 -0800

    renamed app to my-webfolio and rendered a placeholder Header component

---
+++ my-webfolio/src/components/Header.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Header() {
+    return (
+        <header>
+            <h1>Greetings. I am Renzo San Juan. Welcome to my portfolio.</h1>
+        </header>
+    );
+}
