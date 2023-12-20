de9411c66439a6878f4265a9d8cf34f9f6166ada
Renzo San Juan <rcsanjuan96@gmail.com>
2023-12-20 12:26:18 -0800

    added placeholder navbar component, successfully rendered

---
+++ my-webfolio/src/components/Navbar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export default function Navbar() {
+    return (
+        <nav>
+            <ul>
+                <li> <a href="#about-me">About Me</a></li>
+                <li> <a href="#work-1">Western Shootout</a></li>
+                <li> <a href="#work-2">Space Shooter Pro</a></li>
+                <li> <a href="#work-3">Little Hero</a></li>
+                <li> <a href="#work-4">Goodnight King</a></li>
+                <li> <a href="#work-5">Creative Writings</a></li>
+                <li> <a href="#contact">Contact</a></li>
+            </ul>
+        </nav >
+    );
+}
