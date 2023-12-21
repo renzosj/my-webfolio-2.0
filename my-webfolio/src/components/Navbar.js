3094a04ccd9f1b1be3e38cc141658e1abab8b8c9
Renzo San Juan <rcsanjuan96@gmail.com>
2023-12-21 13:12:53 -0800

    added collapsing navbar based on react bootstrap

--- my-webfolio/src/components/Navbar.js
+++ my-webfolio/src/components/Navbar.js
@@ -1,17 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Navbar, Nav, Container } from 'react-bootstrap';
 
-export default function Navbar() {
+export default function NavigationBar() {
+    const [expanded, setExpanded] = useState(false);
+
+    const toggleNavbar = () => {
+        setExpanded(expanded ? false : "expanded");
+    };
+
+    const collapseNavbar = () => {
+        setExpanded(false);
+    };
+
     return (
-        <nav>
-            <ul>
-                <li> <a href="#about-me">About Me</a></li>
-                <li> <a href="#work-1">Western Shootout</a></li>
-                <li> <a href="#work-2">Space Shooter Pro</a></li>
-                <li> <a href="#work-3">Little Hero</a></li>
-                <li> <a href="#work-4">Goodnight King</a></li>
-                <li> <a href="#work-5">Creative Writings</a></li>
-                <li> <a href="#contact">Contact</a></li>
-            </ul>
-        </nav >
+        <Navbar bg="light" expand="lg" expanded={expanded}>
+            <Container>
+                <Navbar.Brand href="#home">Look upon my works, ye mighty</Navbar.Brand>
+                <Navbar.Toggle onClick={toggleNavbar} aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="me-auto">
+                        <Nav.Link href="#about-me" onClick={collapseNavbar}>About Me</Nav.Link>
+                        <Nav.Link href="#work-1" onClick={collapseNavbar}>Movie Database</Nav.Link>
+                        <Nav.Link href="#work-2" onClick={collapseNavbar}>Goodnight, King</Nav.Link>
+                        <Nav.Link href="#work-3" onClick={collapseNavbar}>Mango Tax</Nav.Link>
+                        {/* refactor links later to use loop (map function) */}
+                        <Nav.Link href="#contact" onClick={collapseNavbar}>Contact</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
     );
 }
