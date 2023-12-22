ccc7c912f5151cf24063ba3b5bcc0f1aebd9027b
Renzo San Juan <rcsanjuan96@gmail.com>
2023-12-21 18:06:54 -0800

    added contact form for demo purposes

---
+++ my-webfolio/src/components/Contact.js
@@ -0,0 +1,40 @@
+// Contact.js
+import React from 'react';
+import { Container, Form, Button, Row, Col } from 'react-bootstrap';
+
+export default function Contact() {
+  return (
+    <Container id="contact" className="my-5">
+      <Row className="justify-content-center">
+        <Col xs={12} md={8} lg={6}>
+          <h2>Contact</h2>
+          <Form>
+            <Form.Group className="mb-3" controlId="formName">
+              <Form.Label>Name</Form.Label>
+              <Form.Control type="text" placeholder="Enter name" />
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formEmail">
+              <Form.Label>Email address</Form.Label>
+              <Form.Control type="email" placeholder="Enter email" />
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formNumber">
+              <Form.Label>Phone Number</Form.Label>
+              <Form.Control type="tel" placeholder="Enter phone number" />
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formMessage">
+              <Form.Label>Message</Form.Label>
+              <Form.Control as="textarea" rows={3} />
+            </Form.Group>
+
+            <Button variant="primary" type="submit">
+              Submit
+            </Button>
+          </Form>
+        </Col>
+      </Row>
+    </Container>
+  );
+}
